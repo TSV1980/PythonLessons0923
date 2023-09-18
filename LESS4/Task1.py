0de99a3f29159099afcaa8c3a31bddead37c694b
TSV <sergey.v.t.33@gmail.com>
2023-09-18 14:53:58 +0300

    Task1

--- LESS4/Task1.py
+++ LESS4/Task1.py
@@ -4,3 +4,22 @@
 Пользователь вводит 2 числа. n — кол-во элементов первого множества. 
 m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
 '''
+
+def CreateNewSet(num):
+    new_set = set()
+    for i in range(num):
+        new_set.add(int(input("Введите число для множества: ")))
+    return new_set
+
+
+n = int(input("Введите кол-во элементов первого множества: "))
+n_set = CreateNewSet(n)
+
+m = int(input("Введите кол-во элементов второго множества: "))
+m_set = CreateNewSet(m)
+
+print(*n_set)
+print(*m_set)
+
+s_set = sorted(n_set.intersection(m_set))
+print(*s_set)
